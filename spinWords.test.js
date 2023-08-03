de0180cd9ea9027bf3a7a951a275d4b987fd34e4
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-08-03 14:38:46 +0100

    Test 2 passing: returns a string of words that are 4 letters or less long.

--- spinWords.test.js
+++ spinWords.test.js
@@ -3,4 +3,7 @@ const spinWords = require("./spinWords");
 describe("spinWords", () => {
   test("it returns words that are 4 letters or less long", () =>
     expect(spinWords("hello")).toEqual("hello"));
+
+  test("it returns a string of words that are 4 letters or less long", () =>
+    expect(spinWords("My name is Leah")).toEqual("My name is Leah"));
 });
