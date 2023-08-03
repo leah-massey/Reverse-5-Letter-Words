0ac7856bd1a2970214f7cd628f93a185f670e0a8
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-08-03 14:54:50 +0100

    Test 4 passing:  it reverses any words in a string that are more than 4 letters long.

--- spinWords.test.js
+++ spinWords.test.js
@@ -9,4 +9,7 @@ describe("spinWords", () => {
 
   test("it returns a string of words that are 4 letters or less long", () =>
     expect(spinWords("My name is Leah")).toEqual("My name is Leah"));
+
+  test("it reverses any words in a string that are more than 4 letters long", () =>
+    expect(spinWords("My name is James")).toEqual("My name is semaJ"));
 });
