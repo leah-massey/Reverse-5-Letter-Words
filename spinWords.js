5bfa00cfbbc3633c8a0eb276388253350d71aa84
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-08-03 14:46:27 +0100

    Test 2 changed: a five letter word is reversed.

--- spinWords.js
+++ spinWords.js
@@ -1,5 +1,11 @@
 function spinWords(string) {
-  return string;
+  const letters = string.split("");
+
+  if (letters.length > 4) {
+    return letters.reverse().join("");
+  } else {
+    return string;
+  }
 }
 
 module.exports = spinWords;
--- spinWords.test.js
+++ spinWords.test.js
@@ -2,8 +2,11 @@ const spinWords = require("./spinWords");
 
 describe("spinWords", () => {
   test("it returns words that are 4 letters or less long", () =>
-    expect(spinWords("hello")).toEqual("hello"));
+    expect(spinWords("brew")).toEqual("brew"));
 
-  test("it returns a string of words that are 4 letters or less long", () =>
-    expect(spinWords("My name is Leah")).toEqual("My name is Leah"));
+  test("a five letter word is reversed", () =>
+    expect(spinWords("logical")).toEqual("lacigol"));
+
+  // test("it returns a string of words that are 4 letters or less long", () =>
+  // expect(spinWords("My name is Leah")).toEqual("My name is Leah"));
 });
